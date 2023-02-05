244f021dab3f6c7bd0827cf1b3f892588e92187f
chaynadas <116340466+chaynadas@users.noreply.github.com>
2023-02-05 13:18:18 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,9 +1,9 @@
 function addUser(){
-   player1_name = document.getElementById("Player1_name").Value;
-    player2_name = document.getElementById("Player2_name").Value;
+   player1_name = document.getElementById("player1_name").Value;
+    player2_name = document.getElementById("player2_name").Value;
 
-    localStorage.setItem("Player_1_name" , player1_name);
-    localStorage.setItem("Player_2_name" , player2_name);
+    localStorage.setItem("player_1_name" , player1_name);
+    localStorage.setItem("player_2_name" , player2_name);
 
     window.location = "game_page.html";
 }
